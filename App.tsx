10df24e08db6e384d2b26bc8dfeec7577a998491
lul <luan.melo.bravo10@gmail.com>
2023-04-21 16:18:08 -0300

    Start a Expo app with TS and Tailwind

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
+import { Image } from 'expo-image';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+import { NativeWindStyleSheet } from "nativewind";
+NativeWindStyleSheet.setOutput({
+    default: "native",
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View className='bg-slate-950 flex-1 justify-center items-center'>
+            <Text className='text-slate-50 p-6 font-bold'>LUL Starting Expo Template</Text>
+            <Image className='w-[400] h-[400]' source={require('./source/assets/maxwell-the-cat.gif')}/>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
