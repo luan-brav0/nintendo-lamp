c1cb9de2f9c77dc4f12df659f2f86bb9a9ad0ec6
lul <luan.melo.bravo10@gmail.com>
2023-04-23 14:51:57 -0300

    tailwind custom red added as cRed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./source/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+        "cRed": "#CF010B",
+    },
     extend: {},
   },
   plugins: [],
