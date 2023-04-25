1c3a721da431d2d9d8971a44edeea1915cc92668
lul <luan.melo.bravo10@gmail.com>
2023-04-25 16:42:52 -0300

    metro config

---
+++ source/components/colors.ts
@@ -0,0 +1,14 @@
+import { ColorValue } from "react-native";
+
+export interface Colors {
+  red: ColorValue;
+  white: ColorValue;
+  black: ColorValue;
+  dimRed: ColorValue;
+}
+export const colors: Colors = {
+  red: "#CF010B",
+  white: "#FFFFFF",
+  black: "#000000",
+  dimRed: "#E98F93",
+};
